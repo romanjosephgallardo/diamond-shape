78b6015a432c38138a895aef0f9f1299946ae2cc
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-09-24 21:38:59 +0800

    Add pseudocode

---
+++ diamond_shape.py
@@ -0,0 +1,13 @@
+'''
+Write a Python function named print_diamond that takes an odd integer
+n as an argument and prints a diamond shape with a width of n using the * character.
+Note: If an even number is passed, the function should return "Please provide an odd integer."
+'''
+
+# pseudocode
+
+# define print_diamond function
+# if n is even
+#   print "Please provide an odd integer."
+# if n is odd
+#   print n lines of n characters "*"                                     
