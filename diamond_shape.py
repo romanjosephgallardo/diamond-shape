154d91a8ab6684b08d6be9708330ffcae2358f03
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-09-24 23:07:24 +0800

    Apply minor changes for readability.

--- diamond_shape.py
+++ diamond_shape.py
@@ -1,10 +1,4 @@
-'''
-Write a Python function named print_diamond that takes an odd integer
-n as an argument and prints a diamond shape with a width of n using the * character.
-Note: If an even number is passed, the function should return "Please provide an odd integer."
-'''
-
-# pseudocode
+# Diamond Shape:
 
 # Define print_diamond function
 def print_diamond(n):
@@ -20,7 +14,6 @@ def print_diamond(n):
             for i in range(n//2 -1, -1, -1):
                 print(" " * (n//2 - i) + "*" * (2 * i + 1))
             print()
-
 
 # Test print_diamond function with user input
 try:
